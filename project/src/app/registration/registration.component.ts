84083f1119c711ca4fd667568d32987e9b2d0d17
Yash Nishankar <yashnishankar3102001@gmail.com>
2024-01-13 19:07:18 +0530

    final changes

--- project/src/app/registration/registration.component.ts
+++ project/src/app/registration/registration.component.ts
@@ -18,37 +18,7 @@ export class RegistrationComponent {
   contactForm: FormGroup;
 
 
-  addUser(data: any) {
-    this.backendService.addregisterData(data).subscribe(
-      (userdata) => {
-        console.log("userdata", userdata);
-        Swal.fire({
-          title: 'Successfull',
-          text: "Your registration have been successfull!",
-          icon: 'success',
-          showCancelButton: true,
-          confirmButtonColor: '#3085d6',
-          cancelButtonColor: '#d33',
-          confirmButtonText: 'proceed to login!'
-        }).then((result) => {
-          if (result.isConfirmed) {
-            this.router.navigate(['login']);
-          }
-        })
-      }
-    ), (error: any) => {
-      Swal.fire({
-        icon: 'error',
-        title: 'Oops...',
-        text: 'Something went wrong!',
-        footer: '<a href="">Why do I have this issue?</a>'
-      })
-    }
-    this.clear();
-  }
-
-
-  constructor(private formbuilder: FormBuilder, private backendService: BackendService, private router: Router) {
+  constructor(private formbuilder: FormBuilder, private backendService: BackendService, private router: Router, private http: HttpClient) {
 
     this.contactForm = this.formbuilder.group({
       fullname: [''],
@@ -64,9 +34,62 @@ export class RegistrationComponent {
   }
 
 
+  addUser(data: any) {
+    this.http.get<any>('https://megaproject-9885.onrender.com/user').subscribe(res => {
+      const user = res.find((a: any) => {
+        return a.email === this.userForm.value.email && a.password === this.userForm.value.password
+      })
+      if (user) {
+        Swal.fire({
+          icon: 'info',
+          title: 'Oops...',
+          text: 'User already registered with same email or password!',
+        })
+      } else {
+        this.backendService.addregisterData(data).subscribe(
+          (userdata) => {
+            console.log("userdata", userdata);
+            Swal.fire({
+              title: 'Successfull',
+              text: "Your registration have been successfull!",
+              icon: 'success',
+              showCancelButton: true,
+              confirmButtonColor: '#3085d6',
+              cancelButtonColor: '#d33',
+              confirmButtonText: 'proceed to login!'
+            }).then((result) => {
+              if (result.isConfirmed) {
+                this.router.navigate(['login']);
+              }
+            })
+          }, (error) => {
+            Swal.fire({
+              icon: 'error',
+              title: 'Oops...',
+              text: 'Something went wrong!',
+              footer: 'User registration failed due to invalid email or password'
+            })
+            console.log(error, "error");
+          }
+        )
+      }
+    }, (error: any) => {
+      Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: 'Something went wrong!',
+        footer: 'error from server side occurs'
+      })
+      this.clear();
+      console.log(error, "error");
+    }
+    )
+
+  }
+
+
   clear() {
     this.userForm.reset();
   }
-
 
 }
